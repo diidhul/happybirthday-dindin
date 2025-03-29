dd0e149c684278a70ec3ea1588a261b9dab910fa
diidhul <fadhil.elsafer@gmail.com>
2025-03-30 05:37:36 +0700

    perbaikan redaksi

--- script/main.js
+++ script/main.js
@@ -129,7 +129,7 @@ const animationTimeline = () => {
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=4.5")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
